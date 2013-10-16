551bf8fbc5a8b3a3abf073a8b8e8844f66290c57
Niels Vanderbeke <niels.vanderbeke@gmail.com>
2013-10-16 13:45:15 +0200

    stripped version

---
+++ rpm/jdk/build_jdk7.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+topdir=$(readlink -f $(dirname $0))
+
+rpmbuild -bb --define 'dist .el5' --define "_topdir $topdir" --buildroot=$topdir/BUILDROOT  $topdir/SPECS/jdk7.spec
+rpmbuild -bb --define 'dist .el6' --define "_topdir $topdir" --buildroot=$topdir/BUILDROOT  $topdir/SPECS/jdk7.spec
+
+
